fe50f0ecaa53f6740018542960185ae36c8fc702
Shuqri Shaifuddin <82794055+iirquhs-np@users.noreply.github.com>
2022-04-22 16:44:01 +0800

    completed q5

--- Question5.java
+++ Question5.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Question5
 {
@@ -27,6 +27,30 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
-    
+    int loopLen = in.nextInt();
+
+    ArrayList<Integer> data = new ArrayList<Integer>();
+    HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+    for (int i = 0; i < loopLen; i++) {
+      int number = in.nextInt();
+      data.add(number);
+    }
+    for (int number : data) {
+      if (map.containsKey(number)) {
+        map.put(number, map.get(number) + 1);
+      } else {
+        map.put(number, 1);
+      }
+    }
+
+
+    // sorry cher i googled for this part
+    // i have no idea what this is
+    int highestOccur = (Collections.max(map.values()));
+    for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+      if (entry.getValue()==highestOccur) {
+        System.out.println(entry.getKey());
+      }
+    }
   }
 }
